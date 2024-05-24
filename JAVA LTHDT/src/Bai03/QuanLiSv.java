e1535d6904754a9decb856086fa514702582fa1c
duong van truong <duong van truong@MSI>
2024-05-24 07:59:56 +0700

    hello

--- JAVA LTHDT/src/Bai03/QuanLiSv.java
+++ JAVA LTHDT/src/Bai03/QuanLiSv.java
@@ -4,7 +4,12 @@ public class QuanLiSv {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SinhVien sv;
+		sv = new SinhVien();
+		sv.hoten ="VanTruong";
+		sv.namsinh ="2005";
+		sv.sdt="0799338349";
+		System.out.println("hoten" + sv.hoten + "namsinh" + sv.namsinh + "sdt" + sv.sdt):
 	}
 
 }
--- JAVA LTHDT/src/Bai03/SinhVien.java
+++ JAVA LTHDT/src/Bai03/SinhVien.java
@@ -1,5 +1,7 @@
 package Bai03;
 
 public class SinhVien {
-
+	public string hoten;
+	public int namsinh;
+	public string sdt;
 }
