7c176f2043878a0c666f003382327fab3770666c
duong van truong <duong van truong@MSI>
2024-05-26 17:04:42 +0700

    hello

--- JAVA LTHDT/src/Bai03/Vd2/QuanLySv.java
+++ JAVA LTHDT/src/Bai03/Vd2/QuanLySv.java
@@ -2,4 +2,13 @@ package Bai03.Vd2;
 
 public class QuanLySv {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien sv = new SinhVien();
+		sv.hoten = "Ngoc Truong";
+		sv.namsinh = -2005;
+		sv.sdt = "0967732324";
+		System.out.println(sv);
+	}
+
 }
